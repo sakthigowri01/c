8f3fe04f4c81f158a6edcafe59197fee4cdf6461
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-01-31 14:47:40 +0530

    Create b.c

---
+++ b.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+inta,b;
+clrscr();
+printf("enter a integer ;");
+scanf("%d",&number);
+printf("result;%s",odd even(number));
+return 0;
+}
+char*odd even(int nuber)
+{
+if(number%2==0);
+printf("your number is even number");
+else
+printf("your number is odd number");
+}
